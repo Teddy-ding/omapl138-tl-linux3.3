2292335226c7dad098c65b267c17b2a6ef25292f
Vishwanathrao Badarkhe, Manish <manishv.b@ti.com>
2013-05-22 20:42:53 +0530

    ARM: davinci: lcdk: rename board file name
    Rename board file name from "board-omapl138-hawk.c" to
    "board-omapl138-lcdk.c".
    Also, Modify to remove instances of hawkboard from source
    code.
    
    Signed-off-by: Vishwanathrao Badarkhe, Manish <manishv.b@ti.com>

--- arch/arm/mach-davinci/board-omapl138-lcdk.c
+++ arch/arm/mach-davinci/board-omapl138-lcdk.c
@@ -1,5 +1,5 @@
 /*
- * Hawkboard.org based on TI's OMAP-L138 Platform
+ * TI's OMAP-L138 LCDK Development Board
  *
  * Initial code: Syed Mohammed Khasim
  *
@@ -21,14 +21,14 @@
 #include <mach/da8xx.h>
 #include <mach/mux.h>
 
-#define HAWKBOARD_PHY_ID		"davinci_mdio-0:07"
-#define DA850_HAWK_MMCSD_CD_PIN		GPIO_TO_PIN(3, 12)
-#define DA850_HAWK_MMCSD_WP_PIN		GPIO_TO_PIN(3, 13)
+#define LCDKBOARD_PHY_ID		"davinci_mdio-0:07"
+#define DA850_LCDK_MMCSD_CD_PIN		GPIO_TO_PIN(3, 12)
+#define DA850_LCDK_MMCSD_WP_PIN		GPIO_TO_PIN(3, 13)
 
 #define DA850_USB1_VBUS_PIN		GPIO_TO_PIN(2, 4)
 #define DA850_USB1_OC_PIN		GPIO_TO_PIN(6, 13)
 
-static short omapl138_hawk_mii_pins[] __initdata = {
+static short omapl138_lcdk_mii_pins[] __initdata = {
 	DA850_MII_TXEN, DA850_MII_TXCLK, DA850_MII_COL, DA850_MII_TXD_3,
 	DA850_MII_TXD_2, DA850_MII_TXD_1, DA850_MII_TXD_0, DA850_MII_RXER,
 	DA850_MII_CRS, DA850_MII_RXCLK, DA850_MII_RXDV, DA850_MII_RXD_3,
@@ -37,7 +37,7 @@ static short omapl138_hawk_mii_pins[] __initdata = {
 	-1
 };
 
-static __init void omapl138_hawk_config_emac(void)
+static __init void omapl138_lcdk_config_emac(void)
 {
 	void __iomem *cfgchip3 = DA8XX_SYSCFG0_VIRT(DA8XX_CFGCHIP3_REG);
 	int ret;
@@ -46,7 +46,7 @@ static __init void omapl138_hawk_config_emac(void)
 
 	val = __raw_readl(cfgchip3);
 	val &= ~BIT(8);
-	ret = davinci_cfg_reg_list(omapl138_hawk_mii_pins);
+	ret = davinci_cfg_reg_list(omapl138_lcdk_mii_pins);
 	if (ret) {
 		pr_warning("%s: cpgmac/mii mux setup failed: %d\n",
 			__func__, ret);
@@ -57,7 +57,7 @@ static __init void omapl138_hawk_config_emac(void)
 	__raw_writel(val, cfgchip3);
 	pr_info("EMAC: MII PHY configured\n");
 
-	soc_info->emac_pdata->phy_id = HAWKBOARD_PHY_ID;
+	soc_info->emac_pdata->phy_id = LCDKBOARD_PHY_ID;
 
 	ret = da8xx_register_emac();
 	if (ret)
@@ -67,7 +67,7 @@ static __init void omapl138_hawk_config_emac(void)
 
 /*
  * The following EDMA channels/slots are not being used by drivers (for
- * example: Timer, GPIO, UART events etc) on da850/omap-l138 EVM/Hawkboard,
+ * example: Timer, GPIO, UART events etc) on da850/omap-l138 LCDK EVM,
  * hence they are being reserved for codecs on the DSP side.
  */
 static const s16 da850_dma0_rsv_chans[][2] = {
@@ -115,56 +115,56 @@ static struct edma_rsv_info *da850_edma_rsv[2] = {
 	&da850_edma_cc1_rsv,
 };
 
-static const short hawk_mmcsd0_pins[] = {
+static const short lcdk_mmcsd0_pins[] = {
 	DA850_MMCSD0_DAT_0, DA850_MMCSD0_DAT_1, DA850_MMCSD0_DAT_2,
 	DA850_MMCSD0_DAT_3, DA850_MMCSD0_CLK, DA850_MMCSD0_CMD,
 	DA850_GPIO3_12, DA850_GPIO3_13,
 	-1
 };
 
-static int da850_hawk_mmc_get_ro(int index)
+static int da850_lcdk_mmc_get_ro(int index)
 {
-	return gpio_get_value(DA850_HAWK_MMCSD_WP_PIN);
+	return gpio_get_value(DA850_LCDK_MMCSD_WP_PIN);
 }
 
-static int da850_hawk_mmc_get_cd(int index)
+static int da850_lcdk_mmc_get_cd(int index)
 {
-	return !gpio_get_value(DA850_HAWK_MMCSD_CD_PIN);
+	return !gpio_get_value(DA850_LCDK_MMCSD_CD_PIN);
 }
 
 static struct davinci_mmc_config da850_mmc_config = {
-	.get_ro		= da850_hawk_mmc_get_ro,
-	.get_cd		= da850_hawk_mmc_get_cd,
+	.get_ro		= da850_lcdk_mmc_get_ro,
+	.get_cd		= da850_lcdk_mmc_get_cd,
 	.wires		= 4,
 	.max_freq	= 50000000,
 	.caps		= MMC_CAP_MMC_HIGHSPEED | MMC_CAP_SD_HIGHSPEED,
 	.version	= MMC_CTLR_VERSION_2,
 };
 
-static __init void omapl138_hawk_mmc_init(void)
+static __init void omapl138_lcdk_mmc_init(void)
 {
 	int ret;
 
-	ret = davinci_cfg_reg_list(hawk_mmcsd0_pins);
+	ret = davinci_cfg_reg_list(lcdk_mmcsd0_pins);
 	if (ret) {
 		pr_warning("%s: MMC/SD0 mux setup failed: %d\n",
 			__func__, ret);
 		return;
 	}
 
-	ret = gpio_request_one(DA850_HAWK_MMCSD_CD_PIN,
+	ret = gpio_request_one(DA850_LCDK_MMCSD_CD_PIN,
 			GPIOF_DIR_IN, "MMC CD");
 	if (ret < 0) {
 		pr_warning("%s: can not open GPIO %d\n",
-			__func__, DA850_HAWK_MMCSD_CD_PIN);
+			__func__, DA850_LCDK_MMCSD_CD_PIN);
 		return;
 	}
 
-	ret = gpio_request_one(DA850_HAWK_MMCSD_WP_PIN,
+	ret = gpio_request_one(DA850_LCDK_MMCSD_WP_PIN,
 			GPIOF_DIR_IN, "MMC WP");
 	if (ret < 0) {
 		pr_warning("%s: can not open GPIO %d\n",
-			__func__, DA850_HAWK_MMCSD_WP_PIN);
+			__func__, DA850_LCDK_MMCSD_WP_PIN);
 		goto mmc_setup_wp_fail;
 	}
 
@@ -178,19 +178,19 @@ static __init void omapl138_hawk_mmc_init(void)
 	return;
 
 mmc_setup_mmcsd_fail:
-	gpio_free(DA850_HAWK_MMCSD_WP_PIN);
+	gpio_free(DA850_LCDK_MMCSD_WP_PIN);
 mmc_setup_wp_fail:
-	gpio_free(DA850_HAWK_MMCSD_CD_PIN);
+	gpio_free(DA850_LCDK_MMCSD_CD_PIN);
 }
 
-static irqreturn_t omapl138_hawk_usb_ocic_irq(int irq, void *dev_id);
+static irqreturn_t omapl138_lcdk_usb_ocic_irq(int irq, void *dev_id);
 
-static const short da850_hawk_usb11_pins[] = {
+static const short da850_lcdk_usb11_pins[] = {
 	DA850_GPIO2_4, DA850_GPIO6_13,
 	-1
 };
 
-static struct da8xx_ohci_root_hub omapl138_hawk_usb11_pdata = {
+static struct da8xx_ohci_root_hub omapl138_lcdk_usb11_pdata = {
 	.type	= GPIO_BASED,
 	.method	= {
 		.gpio_method = {
@@ -198,82 +198,82 @@ static struct da8xx_ohci_root_hub omapl138_hawk_usb11_pdata = {
 			.over_current_indicator = DA850_USB1_OC_PIN,
 		},
 	},
-	.board_ocic_handler	= omapl138_hawk_usb_ocic_irq,
+	.board_ocic_handler	= omapl138_lcdk_usb_ocic_irq,
 };
 
-static irqreturn_t omapl138_hawk_usb_ocic_irq(int irq, void *handler)
+static irqreturn_t omapl138_lcdk_usb_ocic_irq(int irq, void *handler)
 {
 	if (handler != NULL)
-		((da8xx_ocic_handler_t)handler)(&omapl138_hawk_usb11_pdata, 1);
+		((da8xx_ocic_handler_t)handler)(&omapl138_lcdk_usb11_pdata, 1);
 	return IRQ_HANDLED;
 }
 
-static __init void omapl138_hawk_usb_init(void)
+static __init void omapl138_lcdk_usb_init(void)
 {
 	u32 cfgchip2;
 
-	/* Setup the Ref. clock frequency for the HAWK at 24 MHz. */
+	/* Setup the Ref. clock frequency for the LCDK at 24 MHz. */
 
 	cfgchip2 = __raw_readl(DA8XX_SYSCFG0_VIRT(DA8XX_CFGCHIP2_REG));
 	cfgchip2 &= ~CFGCHIP2_REFFREQ;
 	cfgchip2 |=  CFGCHIP2_REFFREQ_24MHZ;
 	__raw_writel(cfgchip2, DA8XX_SYSCFG0_VIRT(DA8XX_CFGCHIP2_REG));
 
-	da8xx_board_usb_init(da850_hawk_usb11_pins, &omapl138_hawk_usb11_pdata);
+	da8xx_board_usb_init(da850_lcdk_usb11_pins, &omapl138_lcdk_usb11_pdata);
 
 	return;
 }
 
-static struct davinci_uart_config omapl138_hawk_uart_config __initdata = {
+static struct davinci_uart_config omapl138_lcdk_uart_config __initdata = {
 	.enabled_uarts = 0x7,
 };
 
-static __init void omapl138_hawk_init(void)
+static __init void omapl138_lcdk_init(void)
 {
 	int ret;
 
-	davinci_serial_init(&omapl138_hawk_uart_config);
+	davinci_serial_init(&omapl138_lcdk_uart_config);
 
-	omapl138_hawk_config_emac();
+	omapl138_lcdk_config_emac();
 
 	ret = da850_register_edma(da850_edma_rsv);
 	if (ret)
 		pr_warning("%s: EDMA registration failed: %d\n",
 			__func__, ret);
 
-	omapl138_hawk_mmc_init();
+	omapl138_lcdk_mmc_init();
 
-	omapl138_hawk_usb_init();
+	omapl138_lcdk_usb_init();
 
 	ret = da8xx_register_watchdog();
 	if (ret)
-		pr_warning("omapl138_hawk_init: "
+		pr_warning("omapl138_lcdk_init: "
 			"watchdog registration failed: %d\n",
 			ret);
 }
 
 #ifdef CONFIG_SERIAL_8250_CONSOLE
-static int __init omapl138_hawk_console_init(void)
+static int __init omapl138_lcdk_console_init(void)
 {
-	if (!machine_is_omapl138_hawkboard())
+	if (!machine_is_omapl138_lcdkboard())
 		return 0;
 
 	return add_preferred_console("ttyS", 2, "115200");
 }
-console_initcall(omapl138_hawk_console_init);
+console_initcall(omapl138_lcdk_console_init);
 #endif
 
-static void __init omapl138_hawk_map_io(void)
+static void __init omapl138_lcdk_map_io(void)
 {
 	da850_init();
 }
 
-MACHINE_START(OMAPL138_HAWKBOARD, "AM18x/OMAP-L138 Hawkboard")
+MACHINE_START(OMAPL138_LCDKBOARD, "AM18x/OMAP-L138 lcdk board")
 	.atag_offset	= 0x100,
-	.map_io		= omapl138_hawk_map_io,
+	.map_io		= omapl138_lcdk_map_io,
 	.init_irq	= cp_intc_init,
 	.timer		= &davinci_timer,
-	.init_machine	= omapl138_hawk_init,
+	.init_machine	= omapl138_lcdk_init,
 	.dma_zone_size	= SZ_128M,
 	.restart	= da8xx_restart,
 MACHINE_END
