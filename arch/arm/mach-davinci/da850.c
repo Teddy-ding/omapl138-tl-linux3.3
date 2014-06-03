0bb8d98cd837673d4f75d6838d38f5d8179c8032
Teddy Ding <teddy@tronlong.com>
2014-06-03 17:13:38 +0800

    da850: add GPIO0_X pinmux config
    Signed-off-by: Teddy Ding <teddy@tronlong.com>

--- arch/arm/mach-davinci/da850.c
+++ arch/arm/mach-davinci/da850.c
@@ -663,6 +663,10 @@ static const struct mux_config da850_pins[] = {
 	MUX_CFG(DA850, GPIO7_4,         17,     20,     15,     8,      false)
 	MUX_CFG(DA850, GPIO8_10,        18,	28,	15,     8,      false)
 	MUX_CFG(DA850, GPIO8_1,		3,	28,	15,     4,      false)
+	MUX_CFG(DA850, GPIO0_0,		1,	28,	15,     8,      false)
+	MUX_CFG(DA850, GPIO0_1,		1,	24,	15,     8,      false)
+	MUX_CFG(DA850, GPIO0_2,		1,	20,	15,     8,      false)
+	MUX_CFG(DA850, GPIO0_5,		1,	8,	15,     8,      false)
 	/* eHRPWM0 function */
 	MUX_CFG(DA850,	EHRPWM0_A,	3,	0,	15,	2,	false)
 	MUX_CFG(DA850,	EHRPWM0_B,	3,	4,	15,	2,	false)
