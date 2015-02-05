9381ba8af5d1c55824f5edb36556dfa8547c407d
Teddy Ding <teddy@tronlong.com>
2015-02-05 22:13:36 +0800

    ad7606_ring: change the ad7606_poll_bh_to_ring to ad7606_trigger_handler_th_bh because is cost small time
    Signed-off-by: Teddy Ding <teddy@tronlong.com>

--- drivers/staging/iio/adc/ad7606_ring.c
+++ drivers/staging/iio/adc/ad7606_ring.c
@@ -28,42 +28,19 @@ static irqreturn_t ad7606_trigger_handler_th_bh(int irq, void *p)
 {
 	struct iio_poll_func *pf = p;
 	struct ad7606_state *st = iio_priv(pf->indio_dev);
-
-	gpio_set_value(st->pdata->gpio_convst, 1);
-
-	return IRQ_HANDLED;
-}
-
-/**
- * ad7606_poll_bh_to_ring() bh of trigger launched polling to ring buffer
- * @work_s:	the work struct through which this was scheduled
- *
- * Currently there is no option in this driver to disable the saving of
- * timestamps within the ring.
- * I think the one copy of this at a time was to avoid problems if the
- * trigger was set far too high and the reads then locked up the computer.
- **/
-static void ad7606_poll_bh_to_ring(struct work_struct *work_s)
-{
-	struct ad7606_state *st = container_of(work_s, struct ad7606_state,
-						poll_work);
 	struct iio_dev *indio_dev = iio_priv_to_dev(st);
 	struct iio_buffer *ring = indio_dev->buffer;
 	s64 time_ns;
-	__u8 *buf;
-	int i, count = ring->length;
+	__u8 buf[32];
 	int ret;
 
-
-	buf = kzalloc(ring->access->get_bytes_per_datum(ring),
-		      GFP_KERNEL);
-	if (buf == NULL)
-		return;
-
-	disable_irq(st->irq);
-
-	for (i = 0; i < count; i++) {
 	gpio_set_value(st->pdata->gpio_convst, 1);
+
+	if (ring->access->get_bytes_per_datum(ring) > sizeof(buf)) {
+		printk(KERN_ERR "buf default size too small requese %d\n",
+			ring->access->get_bytes_per_datum(ring));
+		goto done;
+	}
 
 	if (gpio_is_valid(st->pdata->gpio_frstdata)) {
 		ret = st->bops->read_block(st->dev, 1, buf);
@@ -89,8 +66,6 @@ static void ad7606_poll_bh_to_ring(struct work_struct *work_s)
 			goto done;
 	}
 
-	gpio_set_value(st->pdata->gpio_convst, 0);
-
 	time_ns = iio_get_time_ns();
 
 #if 0
@@ -101,13 +76,76 @@ static void ad7606_poll_bh_to_ring(struct work_struct *work_s)
 
 	ring->access->store_to(indio_dev->buffer, buf, time_ns);
 
-	ndelay(100);
+done:
+	gpio_set_value(st->pdata->gpio_convst, 0);
+	iio_trigger_notify_done(indio_dev->trig);
+
+	return IRQ_HANDLED;
+}
+
+/**
+ * ad7606_poll_bh_to_ring() bh of trigger launched polling to ring buffer
+ * @work_s:	the work struct through which this was scheduled
+ *
+ * Currently there is no option in this driver to disable the saving of
+ * timestamps within the ring.
+ * I think the one copy of this at a time was to avoid problems if the
+ * trigger was set far too high and the reads then locked up the computer.
+ **/
+static void ad7606_poll_bh_to_ring(struct work_struct *work_s)
+{
+#if 0
+	struct ad7606_state *st = container_of(work_s, struct ad7606_state,
+						poll_work);
+	struct iio_dev *indio_dev = iio_priv_to_dev(st);
+	struct iio_buffer *ring = indio_dev->buffer;
+	s64 time_ns;
+	__u8 *buf;
+	int ret;
+
+	buf = kzalloc(ring->access->get_bytes_per_datum(ring),
+		      GFP_KERNEL);
+	if (buf == NULL)
+		return;
+
+	if (gpio_is_valid(st->pdata->gpio_frstdata)) {
+		ret = st->bops->read_block(st->dev, 1, buf);
+		if (ret)
+			goto done;
+		if (!gpio_get_value(st->pdata->gpio_frstdata)) {
+			/* This should never happen. However
+			 * some signal glitch caused by bad PCB desgin or
+			 * electrostatic discharge, could cause an extra read
+			 * or clock. This allows recovery.
+			 */
+			ad7606_reset(st);
+			goto done;
+		}
+		ret = st->bops->read_block(st->dev,
+			st->chip_info->num_channels - 1, buf + 2);
+		if (ret)
+			goto done;
+	} else {
+		ret = st->bops->read_block(st->dev,
+			st->chip_info->num_channels, buf);
+		if (ret)
+			goto done;
 	}
+
+	time_ns = iio_get_time_ns();
+
+#if 0
+	if (ring->scan_timestamp)
+		*((s64 *)(buf + ring->access->get_bytes_per_datum(ring) -
+			  sizeof(s64))) = time_ns;
+#endif
+
+	ring->access->store_to(indio_dev->buffer, buf, time_ns);
 done:
 	gpio_set_value(st->pdata->gpio_convst, 0);
 	iio_trigger_notify_done(indio_dev->trig);
 	kfree(buf);
-	enable_irq(st->irq);
+#endif
 }
 
 static const struct iio_buffer_setup_ops ad7606_ring_setup_ops = {
